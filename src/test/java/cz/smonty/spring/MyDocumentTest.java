8c439d5f2f423c99e23757697546f694d2a3c9c8
Peter Smatana <petersmatana@gmail.com>
2016-11-20 16:49:45 +0100

    nejde mi naimportovat xml s "kontextem" - to xml kde popisu beany a pak to predhodim tride XYZXMLApplicationContext("konfigurak.xml"). mam takovej pocit, ze spring ten soubor ocekava kdesi v META-INF...

--- src/test/java/cz/smonty/spring/MyDocumentTest.java
+++ src/test/java/cz/smonty/spring/MyDocumentTest.java
@@ -4,15 +4,28 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import cz.smonty.model.Type;
 import cz.smonty.model.Document;
 import cz.smonty.service.MySearchEngine;
+import cz.smonty.service.SearchEngine;
 
 public class MyDocumentTest {
 
-	private MySearchEngine engine = new MySearchEngine();
+	private SearchEngine engine;
+	private ClassPathXmlApplicationContext context;
+	private Type documentType;
+	
+	@Before
+	public void init() {
+		// /home/smonty/Documents/sprint/SpringExamples/src/main/java/cz/smonty/service/
+		context = new ClassPathXmlApplicationContext("BeanConf.xml");
+		engine = context.getBean(SearchEngine.class);
+		documentType = context.getBean(Type.class);
+	}
 	
 	@Test
 	public void testFindByType() {
